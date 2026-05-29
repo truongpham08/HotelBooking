00a7598b9dbc6705d98fb937ad9eb292cf86b7f0
BaoHan260304 <quannmhe182433@fpt.edu.vn>
2026-05-29 13:42:48 +0700

    test thử

--- HotelBookingUi/src/pages/Auth/LoginPage.jsx
+++ HotelBookingUi/src/pages/Auth/LoginPage.jsx
@@ -4,7 +4,7 @@ const LoginPage = () => (
   <div className="min-h-screen bg-gray-50 flex items-center justify-center">
     <div className="text-center p-10 bg-white rounded-xl shadow">
       <h1 className="text-3xl font-bold text-gray-800">Trang Đăng Nhập</h1>
-      <p className="mt-3 text-gray-500">Thành viên 1 triển khai tại <code>src/pages/Auth/LoginPage.jsx</code></p>
+      <p className="mt-3 text-gray-500">Thành viên 1 triển khai tại đây <code>src/pages/Auth/LoginPage.jsx</code></p>
     </div>
   </div>
 );
